071fd483065b08815523d0651c8ab9ca45c35d7a
gihanrv <gihanrangana.mr@gmail.com>
2023-04-19 00:17:40 +0530

    Update ansible roles for nginx deployment

--- modules/aws/ec2/conf/ec2-dev.sh
+++ modules/aws/ec2/conf/ec2-dev.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -xe
+#!/bin/bash 
+set -e 
 sudo bash -c 'exec > >( tee /var/log/user-data.log|logger -t user-data -s 2>/var/log/userdata.log ) 2>&1'
 sudo apt update && sudo apt install software-properties-common && sudo add-apt-repository --yes --update ppa:ansible/ansible &&
 sudo apt install ansible  -y 
+sudo git clone -b master https://github.com/gihanrv/yolo-devops.git /tmp 
+#sudo git clone -b ${git_branch} ${git_repo_url} /tmp 
