9aa0355549dae2cc631010cdae5aa05c101d7537
Gihan Rangana <gihanrangana.mr@gmail.com>
2023-04-20 21:26:52 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd yolo-dev-use1/
 echo "This will run terragrunt run-all destroy and pass 'y'  destroy changes"
-terragrunt run-all apply << EOF
+terragrunt run-all destroy << EOF
 y
 EOF
