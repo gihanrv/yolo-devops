31a503f8c71b45fa372d2982688332beaa0b4361
gihanrv <gihanrangana.mr@gmail.com>
2023-04-20 11:34:44 +0530

    Add DNS name as output

--- modules/aws/elb/outputs.tf
+++ modules/aws/elb/outputs.tf
@@ -7,3 +7,8 @@ output "alb_id" {
   description = "Id of the ALB"
   value       = aws_lb.lb[*].id
 }
+
+output "alb_dns_name" {
+  description = "dns_name of the ALB"
+  value       = aws_lb.lb[*].dns_name
+}
